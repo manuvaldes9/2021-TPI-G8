f62dfef9b21c231bae1ec0f554b53c4d6d490abd
Martin Getzel <martingetzel@gmail.com>
2021-05-22 14:14:03 -0300

    funcion getNotificacion en proceso

--- run.js
+++ run.js
@@ -1,6 +1,4 @@
 const empresas = require('./lib/grupo8.js')
 const api = 'https://60a837a28532520017ae5b4d.mockapi.io/regimen';
 
-const succeed = function(pepito) {return pepito};
-
-console.log(empresas.getNotificacion(api));
+empresas.getNotificacion(api).then( res => console.log(res));
