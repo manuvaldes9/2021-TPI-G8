f821022b77599445bba4cc0d2af955c1a5e00462
Martin Getzel <martingetzel@gmail.com>
2021-05-22 23:50:05 -0300

    Selección por id + cambios menores

--- run.js
+++ run.js
@@ -1,4 +1,6 @@
 const empresas = require('./lib/grupo8.js')
 const api = 'https://60a837a28532520017ae5b4d.mockapi.io/regimen';
+const id = '2';
 
-empresas.getNotificacion(api).then( res => console.log(res));
+
+var x = empresas.getNotificacion(api,id).then(x => console.log(x));
