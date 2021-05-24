5ddc28e6d7299035de44d63d155d298f2592a4f1
Martin Getzel <martingetzel@gmail.com>
2021-05-24 13:22:24 -0300

    generación y post de regímenes

--- run.js
+++ run.js
@@ -1,6 +1,23 @@
-const empresas = require('./lib/grupo8.js')
-const api = 'https://60a837a28532520017ae5b4d.mockapi.io/regimen';
+const fetch = require('node-fetch');
+const empresas = require('./lib/grupo8.js');
+const api_regimen = 'https://60a829368532520017ae5a5f.mockapi.io/regimen';
+const api_notificacion = 'https://60a829368532520017ae5a5f.mockapi.io/notificaciones';
 const id = '2';
 
+//GET a los datos de la API
+empresas.getNotificacion(api_notificacion, id).then(sal => console.log(sal)); 
+// Al parecer, para usar los resultados de esta función es obligatorio hacerlo dentro del .then() -- Habría que ver si le seguimos dando vueltas o lo dejamos ahí y ya fue
 
-var x = empresas.getNotificacion(api,id).then(x => console.log(x));
+
+//Generación de un regímen
+var fecha = "2025-05-31";
+var regimen = empresas.generarRegimen(2025, 10, [{ registroNro: 1 }, { registroNro: 2 }, { registroNro: 3 }], fecha, "15362585915", "Ricardo Ramirez e Hijos S.R.L");
+console.log(regimen);
+// Está hecha la generación del regímen, pero falta la generación de los registros, debería hacerse de la misma manera que los regímenes.
+// Tenemos que poder armar una lista de registros también, hay que ver si eso es una función o le dejamos el trabajo al usuario de la librería.
+// Podemos tener también una función como agregarRegistro(regimen, registro) e ir agregándolos después de creados.
+
+
+//Carga de un regimen a la API
+empresas.postRegimen(api_regimen, regimen);
+
