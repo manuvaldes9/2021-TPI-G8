4d4f5b3bd4ededbdd395c08f28ea078868651d23
FeR35300 <Facevedo326@gmail.com>
2021-05-25 12:26:16 -0300

    Actualizo rama back con archivos de controlador, database y models

---
+++ tpi-g8/src/app/database/index.js
@@ -0,0 +1,71 @@
+const dbConfig = require("./db.config.js");
+const Sequelize = require("sequelize");
+const productos = require("../models/productos.js");
+const produccion = require("../models/produccion.js");
+const ventas = require("../models/ventas.js");
+
+//Instancia de sequelize para la conexion a la BBDD
+const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
+  host: dbConfig.HOST,
+  dialect: dbConfig.dialect,
+
+  pool: {
+    max: dbConfig.pool.max,
+    min: dbConfig.pool.min,
+    acquire: dbConfig.pool.acquire,
+    idle: dbConfig.pool.idle
+  },
+  //Esta parte es porque nuestra BBDD usa certificación SSL 
+  ssl: true,
+  dialectOptions: {
+    ssl: {
+      require: true
+    }
+  }
+});
+
+
+//Autenticacion a la BBDD
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established');
+  })
+  .catch((err) => {
+    console.error('Unable to connecto to db: ' + err);
+  });
+
+
+  //Definicion de los modelos a utilizar
+  const Productos = productos(sequelize, Sequelize);
+  const Produccion = produccion(sequelize, Sequelize);
+  const Ventas = ventas(sequelize, Sequelize);
+
+
+  //Seccion de CONSULTAS
+  exports.Productos.findAll({ attributes: ['EAN','nombre','precioKG','stock'] })
+  .then(data => {
+    const consulta = JSON.stringify(data); //Esto nos devuelve el resultado de la consulta escrito como JSON pero en formato STRING
+    console.log(JSON.parse(consulta)) //Aca transformo el resultado de la consulta a JSON
+  })
+  .catch(err => {
+    console.log(err)
+  });
+
+  Produccion.findAll({ attributes: ['idProduccion','idProducto','cantidad','fecha'] })
+  .then(data => {
+    const consulta = JSON.stringify(data); //Esto nos devuelve el resultado de la consulta escrito como JSON pero en formato STRING
+    console.log(JSON.parse(consulta)) //Aca transformo el resultado de la consulta a JSON
+  })
+  .catch(err => {
+    console.log(err)
+  });
+
+  Ventas.findAll({ attributes: ['idVenta','fecha','idDetalle'] })
+  .then(data => {
+    const consulta = JSON.stringify(data); //Esto nos devuelve el resultado de la consulta escrito como JSON pero en formato STRING
+    console.log(JSON.parse(consulta)) //Aca transformo el resultado de la consulta a JSON
+  })
+  .catch(err => {
+    console.log(err)
+  })
