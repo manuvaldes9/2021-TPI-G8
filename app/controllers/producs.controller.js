8b975083f03f983702c1f1fd7e67bc1b577ff9a8
FeR35300 <Facevedo326@gmail.com>
2021-06-01 16:08:48 -0300

    Metodo getProductos funcional

--- app/controllers/producs.controller.js
+++ app/controllers/producs.controller.js
@@ -4,29 +4,41 @@ const Producs = db.producs;
 const Op = db.Sequelize.Op;
 
 exports.findAll = (req, res) => {
-  var fecha1 = new Date();
-  var fecha2 = new Date();
-  fecha2.setMonth(fecha1.getMonth()-1);
-  
-  const Producs = db.producs;
-  const Op = db.Sequelize.Op;
-  Producs.findAll({ attributes : ['nombre','EAN','precioKG','cantidadP','cantidadV','fecha','unidad'],
-      where :{
-          fecha:{
-              [Op.between]:[fecha2,fecha1]
-          }
-      }
-  
-    })
-    .then(data => {
-      const consulta = JSON.stringify(data); //Esto nos devuelve el resultado de la consulta escrito como JSON pero en formato STRING
-      return(consulta);
-      //console.log(JSON.parse(consulta)) //Aca transformo el resultado de la consulta a JSON
-    })
-    .catch(err => {
-      console.log(err)
-    });
-};
+        var fecha1 = new Date();
+        var fecha2 = new Date();
+        fecha2.setMonth(fecha1.getMonth() - 1);
+        const db = require("../models");
+        const Producs = db.producs;
+        const Op = db.Sequelize.Op;
+        try {
+            Producs.findAll({
+                attributes: [
+                    "nombre",
+                    "EAN",
+                    "precioKG",
+                    "cantidadP",
+                    "cantidadV",
+                    "unidad",
+                ],
+                where: {
+                    fecha: {
+                        [Op.between]: [fecha2, fecha1],
+                    },
+                },
+            })
+                .then((data) => {
+                    // const consulta = JSON.stringify(data); //Esto nos devuelve el resultado de la consulta escrito como JSON pero en formato STRING
+                    // res = consulta;
+                    res.json(data);
+                    //console.log(JSON.parse(consulta)) //Aca transformo el resultado de la consulta a JSON
+                })
+                .catch((err) => {
+                    console.log(err);
+                });
+        } catch (e) {
+            console.error(e);
+        }
+    };
 
 
 
--- app/routes/routes.js
+++ app/routes/routes.js
@@ -1,21 +1,9 @@
 module.exports = app => {
-    // const produccion = require("../controllers/produccion.controller.js");
-    // const ventas = require("../controllers/ventas.controller.js");
-    // const productos = require("../controllers/productos.controller.js")
     const usuarios = require("../controllers/usuarios.controller.js");
     const producs = require("../controllers/producs.controller.js");
     const libreria = require("../controllers/libreria.controller.js");
     
     var router = require("express").Router();
-
-    // //Recupera todas las producciones
-    // router.get("/", produccion.findAll);
-
-    // //Recupera todas las ventas
-    // router.get("/", ventas.findAll);
-
-    // //Recupera todos los productos
-    // router.get("/", productos.findAll);
 
     //Recupera todos los usuarios
     router.get("/", usuarios.findAll);
@@ -25,14 +13,6 @@ module.exports = app => {
 
     router.get("/", libreria.getNotificacion);
 
-    app.get("/hola",(req,res) => {
-        try{
-            producs.findAll().then(result => res.write(result))
-        }
-        catch (error){
-            res.json(error);
-        }
-        //res.json( producs.findAll())
-    });
-    
+    app.get("/getProductos", producs.findAll)
+
 }
