2f34e78c24e3cd6abf7cc15af22d50ab71f1d5f7
FeR35300 <Facevedo326@gmail.com>
2021-07-15 13:41:53 -0300

    login funcional

--- react-crud/src/App.js
+++ react-crud/src/App.js
@@ -1,63 +1,79 @@
+
 import React, { Component } from "react";
 import './App.css';
 import AuthService from "./services/auth.service";
 import { Link, Switch, Route } from "react-router-dom";
 import ProductList from './components/servicios.component'
 import PrincipalPage from "./components/principal.component";
+
+import { useAuth0 } from "@auth0/auth0-react";
 import Login from "./components/login.component";
+import {LoginButton} from './components/login.component'
+import {LogoutButton} from "./components/logout.component";
+import {Profile} from "./components/profile.component";
+import {ServiciosButton} from "./components/botonservicios.component";
+function App() {
+// class App extends Component {
+  // constructor(props) {
+  //   super(props);
+    
+  //   this.state = {
+  //     showServicios: false,
+  //     showPrincipal: false
+  //   };
+  // }
+  // componentDidMount() {
+  //   const user = AuthService.getCurrentUser();
+  // }
+  // if (user) {
+  //   this.setState({
+  //     showServicios: user,
+  //     showPrincipal: user
+  //   });
+  // }
 
-
-class App extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      showServicios: false,
-      showPrincipal: false
-    };
-  }
-  componentDidMount() {
-    const user = AuthService.getCurrentUser();
-  }
-  if (user) {
-    this.setState({
-      showServicios: user,
-      showPrincipal: user
-    });
-  }
-
-
-  render() {
-    const { showPrincipal, showServicios } = this.state;
+  // render() {
+    const{ isAuthenticated } = useAuth0();
+    // const { showPrincipal, showServicios } = this.state;
+    
       return (
-        
       <div class= "text-center">
         <div class="cover-container d-flex h-100 p-3 mx flex-column">
           <header class="masthead mb">
-                  <div class="inner">
+              <div class="inner">
                   <h4 class="masthead-brand ">
                     <a href="/principal" className="navbar-brand">
                          Corralon Nak
                     </a>
+                    {isAuthenticated ? (
+                      <>
+                        <Profile/>
+                        <LogoutButton/>
+                      </>
+                    ) : (
+                      <LoginButton/>
+                    )}
                     </h4>
-                    <nav class="nav nav-masthead justify-content-center">
-                    {showPrincipal && (
-                       <a class="nav-link active">
-                          <Link to={"/principal"}>
-                              Principal
-                         </Link>
-                       </a>
-                    )}
-                    {showServicios && (
+                    {isAuthenticated ? (
+                    <> 
+                      <nav class="nav nav-masthead justify-content-center">
                         <a class="nav-link active">
-                          <Link to={"/servicios"}>
-                              Servicios
+                            <Link to={"/principal"}>
+                                Principal
                           </Link>
-                       </a>
-                    )}
-                  </nav>
-                  </div>
-              </header>        
+                        </a>
+                        <a class="nav-link active">
+                            <Link to={"/servicios"}>
+                                Servicios
+                            </Link>
+                        </a>                    
+                      </nav>
+                    </>
+                  ) : (
+                    <LoginButton/>
+                  )}
+              </div>
+          </header>        
           <div className="container mt-3">
             <Switch>
               <Route exact path={["/servicios"]} component={ProductList} />
@@ -68,7 +84,7 @@ class App extends Component {
         </div>
       </div>
       );
-  }
+  // }
 }
 
 export default App;
--- react-crud/src/index.js
+++ react-crud/src/index.js
@@ -1,13 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Auth0Provider } from '@auth0/auth0-react'
 import { HashRouter } from "react-router-dom";
 
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
+  
   <HashRouter>
-    <App />
+    <React.StrictMode>
+      <Auth0Provider domain="grupo8-tpi.us.auth0.com" clientId="CXKqDkfXlEjROqsE4X6vTvElHHuqYBxc" redirectUri={window.location.origin}>
+      <App />
+      </Auth0Provider>
+    </React.StrictMode>
   </HashRouter>,
   document.getElementById("root")
 );
